2400f34e2fc289fb17dbaabce457699b20774df1
= <juanlgu27@gmail.com>
2022-01-28 19:52:28 -0600

    movimiento

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Public files
+
+    public string Name;
+    public int order;
+    public int life = 100;
+    public int attack = 10;
+
+    #endregion
+}
